32f1894552a1abfdf1ce1c3845d812db6dd9f7f6
svr <svr.work@protonmail.com>
2021-08-24 15:41:47 +0300

    реализация CalcView в CalcActivity

--- app/src/main/java/com/geekbrains/calc/CalcActivity.kt
+++ app/src/main/java/com/geekbrains/calc/CalcActivity.kt
@@ -2,19 +2,22 @@ package com.geekbrains.calc
 
 import android.os.Bundle
 import android.widget.GridLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class CalcActivity : AppCompatActivity(), CalcView {
     val calcModel: CalcModel = CalcModelImpl()
     val calcPresenter: CalcPresenter = CalcPresenterImpl(this, calcModel)
+    var displayView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<GridLayout>(R.id.grid).setBackgroundResource(R.drawable.bg)
+        displayView = findViewById<TextView>(R.id.calc_display_view)
     }
 
     override fun setCalcDisplay(text: String) {
-        TODO("Not yet implemented")
+        displayView!!.setText(text)
     }
 }
